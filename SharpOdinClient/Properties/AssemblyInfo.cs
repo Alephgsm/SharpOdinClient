ddae79b76840a1ff0666288885cfdcfa731cc926
AsgharSo <AsgharSo@DESKTOP-H38B28I>
2022-11-10 02:48:04 -0800

    updated assambly information

--- SharpOdinClient/Properties/AssemblyInfo.cs
+++ SharpOdinClient/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SharpOdinClient")]
-[assembly: AssemblyDescription("samsung download mode protocol for c# language")]
+[assembly: AssemblyDescription("SharpOdinClient is a .NET library that allows .NET applications to communicate with samsung android devices in download mode. A suitable client for flash(image , tar.md5 , lz4), getting info and implementing other features. It provides a .NET implementation of the odin protocol.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gsm Alphabet - Alephgsm.com")]
 [assembly: AssemblyProduct("SharpOdinClient")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.9")]
-[assembly: AssemblyFileVersion("1.0.0.9")]
+[assembly: AssemblyVersion("1.0.1.9")]
+[assembly: AssemblyFileVersion("1.0.1.9")]
